18153b23c49b6fac3c0078dfe1cf39b07f7001ff
ciclo6moviles2024idat <juankuga@Juans-MacBook-Pro.local>
2024-06-24 20:33:06 -0500

    Snackbar component

--- app/src/main/java/pe/idat/appmenucomponents/MenuScreen.kt
+++ app/src/main/java/pe/idat/appmenucomponents/MenuScreen.kt
@@ -1,27 +1,36 @@
 package pe.idat.appmenucomponents
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarColors
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -51,11 +60,34 @@ fun EjemploToolBar(){
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun EjemploScaffold(){
-    Scaffold(topBar = { EjemploToolBar() }
-            ){
+    var estado = remember {
+        SnackbarHostState()
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(topBar = { EjemploToolBar()},
+        snackbarHost = {
+            SnackbarHost(hostState = estado)
+        },
+        content = {
+            Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center){
+                Button(onClick = {
+                    coroutineScope.launch {
+                        estado.showSnackbar(
+                            "Hola clickeaste el botton",
+                            actionLabel = "OK",
+                            duration = SnackbarDuration.Long)
+                    }
+
+                }) {
+                  Text(text = "Mostrar SnackBar")
+                }
+            }
+        })
 
     }
-}
+
 
 
 
